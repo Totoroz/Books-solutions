a3b8807b6ec7b4203e6394aaba9e88fe48c4166b
Anton Antonov <anton.synd.antonov@gmail.com>
2014-02-13 15:18:10 +0200

    fixed lack of comment blocks

--- C++ for Everyone, 2nd Ed, Horstmann/Chapter3/P3-17.cpp
+++ C++ for Everyone, 2nd Ed, Horstmann/Chapter3/P3-17.cpp
@@ -1,12 +1,12 @@
-Write a program that prompts for the day and month of the user’s birthday and then
-prints a horoscope.Make up fortunes for programmers, like this:
-Please enter your birthday(month and day) : 6 16
-Gemini are experts at figuring out the behavior of complicated programs.
-You feel where bugs are coming from and then stay one step ahead.Tonight,
-your style wins approval from a tough critic.
-Each fortune should contain the name of the astrological sign. (You will find the
-names and date ranges of the signs at a distressingly large number of sites on the
-Internet.)
+// Write a program that prompts for the day and month of the userï¿½s birthday and then
+// prints a horoscope.Make up fortunes for programmers, like this:
+// Please enter your birthday(month and day) : 6 16
+// Gemini are experts at figuring out the behavior of complicated programs.
+// You feel where bugs are coming from and then stay one step ahead.Tonight,
+// your style wins approval from a tough critic.
+// Each fortune should contain the name of the astrological sign. (You will find the
+// names and date ranges of the signs at a distressingly large number of sites on the
+// Internet.)
 
 #include<iostream>
 
