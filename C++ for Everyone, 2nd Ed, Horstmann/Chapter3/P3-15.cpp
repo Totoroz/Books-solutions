8ceaeb00645385525549cf6b68830608523d65db
Anton Antonov <anton.synd.antonov@gmail.com>
2014-02-13 15:17:38 +0200

    fixed lack of comment blocks

--- C++ for Everyone, 2nd Ed, Horstmann/Chapter3/P3-15.cpp
+++ C++ for Everyone, 2nd Ed, Horstmann/Chapter3/P3-15.cpp
@@ -1,9 +1,9 @@
-Write a program to simulate a bank transaction.There are two bank accounts : check -
-ing and savings.First, ask for the initial balances of the bank accounts; reject negative
-balances.Then ask for the transactions; options are deposit, withdrawal, and trans -
-fer.Then ask for the account; options are checking and savings.Then ask for the
-amount; reject transactions that overdraw an account.At the end, print the balances
-of both accounts.
+// Write a program to simulate a bank transaction.There are two bank accounts : check -
+// ing and savings.First, ask for the initial balances of the bank accounts; reject negative
+// balances.Then ask for the transactions; options are deposit, withdrawal, and trans -
+// fer.Then ask for the account; options are checking and savings.Then ask for the
+// amount; reject transactions that overdraw an account.At the end, print the balances
+// of both accounts.
 
 #include<iostream>
 
