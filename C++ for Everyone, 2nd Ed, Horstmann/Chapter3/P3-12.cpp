6ed9b9208007dcdcdbf412a8857751d2d42c2d81
Anton Antonov <anton.synd.antonov@gmail.com>
2014-02-13 15:16:52 +0200

    fixed lack of comment blocks

--- C++ for Everyone, 2nd Ed, Horstmann/Chapter3/P3-12.cpp
+++ C++ for Everyone, 2nd Ed, Horstmann/Chapter3/P3-12.cpp
@@ -1,19 +1,19 @@
-When two points in time are compared, each given as hours(in military time,
-	rang ing from 0 and 23) and minutes, the following pseudocode determines which
-	comes first.
-	If hour1 < hour2
-	time1 comes first.
-	Else if hour1 and hour2 are the same
-	If minute1 < minute2
-	time1 comes first.
-	Else if minute1 and minute2 are the same
-	time1 and time2 are the same.
-	Else
-	time2 comes first.
-	Else
-	time2 comes first.
-	Write a program that prompts the user for two points in time and prints the time that
-	comes first, then the other time.
+// When two points in time are compared, each given as hours(in military time,
+// 	rang ing from 0 and 23) and minutes, the following pseudocode determines which
+// 	comes first.
+// 	If hour1 < hour2
+// 	time1 comes first.
+// 	Else if hour1 and hour2 are the same
+// 	If minute1 < minute2
+// 	time1 comes first.
+// 	Else if minute1 and minute2 are the same
+// 	time1 and time2 are the same.
+// 	Else
+// 	time2 comes first.
+// 	Else
+// 	time2 comes first.
+// 	Write a program that prompts the user for two points in time and prints the time that
+// 	comes first, then the other time.
 
 #include<iostream>
 
