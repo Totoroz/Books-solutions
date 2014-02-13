735da2023a7bdbf96c4949fcb2e800b01025b8fc
Anton Antonov <anton.synd.antonov@gmail.com>
2014-02-13 15:17:52 +0200

    fixed lack of comment blocks

--- C++ for Everyone, 2nd Ed, Horstmann/Chapter3/P3-16.cpp
+++ C++ for Everyone, 2nd Ed, Horstmann/Chapter3/P3-16.cpp
@@ -1,8 +1,8 @@
-Write a program that reads in the name and salary of an employee.Here the salary
-will denote an hourly wage, such as $9.25.Then ask how many hours the employee
-worked in the past week.Be sure to accept fractional hours.Any overtime work
-(over 40 hours per week) is paid at 150 percent of the regular wage.Compute the
-pay.Print a paycheck for the employee.
+// Write a program that reads in the name and salary of an employee.Here the salary
+// will denote an hourly wage, such as $9.25.Then ask how many hours the employee
+// worked in the past week.Be sure to accept fractional hours.Any overtime work
+// (over 40 hours per week) is paid at 150 percent of the regular wage.Compute the
+// pay.Print a paycheck for the employee.
 
 #include<iostream>
 
