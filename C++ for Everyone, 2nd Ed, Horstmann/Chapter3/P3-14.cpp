909098b5c33de083527a35555d4ba4e01e3219a6
Anton Antonov <anton.synd.antonov@gmail.com>
2014-02-13 15:17:11 +0200

    fixed lack of comment blocks

--- C++ for Everyone, 2nd Ed, Horstmann/Chapter3/P3-14.cpp
+++ C++ for Everyone, 2nd Ed, Horstmann/Chapter3/P3-14.cpp
@@ -1,9 +1,9 @@
-Write a program that reads in two floating - point numbers and tests whether they are
-the same up to two decimal places.Here are two sample runs.
-Enter two floating - point numbers : 2.0 1.99998
-They are the same up to two decimal places.
-Enter two floating - point numbers : 2.0 1.98999
-They are different.
+// Write a program that reads in two floating - point numbers and tests whether they are
+// the same up to two decimal places.Here are two sample runs.
+// Enter two floating - point numbers : 2.0 1.99998
+// They are the same up to two decimal places.
+// Enter two floating - point numbers : 2.0 1.98999
+// They are different.
 
 #include<iostream>
 #include<cmath>
