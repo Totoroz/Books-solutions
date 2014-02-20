127b366024f2c1474c3b603fb9d4d0e7b4df0100
syndbg <syndbe@gmail.com>
2014-02-20 22:44:58 +0200

    init

---
+++ Python For Everyone, Horstmann/Chapter6-Lists/R6.1A.py
@@ -0,0 +1,10 @@
+# Given the list values = [] , write code that fills the list with each set of numbers below.
+# a.1     2   3   4   5   6   7   8   9   10
+
+
+list = []
+
+for i in range(11):
+    list.append(i)
+
+print(list)
