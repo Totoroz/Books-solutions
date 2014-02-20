f769a188b1a753b2550294a86470202d6203452c
syndbg <syndbe@gmail.com>
2014-02-20 22:59:52 +0200

    init

---
+++ Python For Everyone, Horstmann/Chapter6-Lists/R6.1B.py
@@ -0,0 +1,9 @@
+# Given the list values = [] , write code that fills the list with each set of numbers below.
+# b.0   2   4   6   8   10   12   14   16   18
+
+list = []
+
+for i in range(0, 21, 2):
+    list.append(i)
+
+print(list)
