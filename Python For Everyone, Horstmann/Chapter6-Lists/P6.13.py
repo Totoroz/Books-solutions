d68e7af0c34dfc03ebed675ac96f865242302bea
syndbg <syndbe@gmail.com>
2014-02-23 00:54:12 +0200

    What am I committing..

---
+++ Python For Everyone, Horstmann/Chapter6-Lists/P6.13.py
@@ -0,0 +1,58 @@
+# Write a function
+#     def sameElements(a, b)
+# that checks whether two lists have the same elements in some order, with the same
+# multiplicities. For example,
+#             1  4  9  16  9  7  4  9  11
+#     and
+#             11  1  4  9  16  9  7  4  9
+#
+#     would be considered identical, but
+#
+#             1  4  9  16  9  7  4  9  11
+#     and
+#             11  11  7  9  16  4  1  4  9
+#
+# would not. You will probably need one or more helper functions.
+
+# FUNCTIONS
+def sameSet(listA, listB):
+    removeDuplicates(listA)
+    removeDuplicates(listB)
+
+    lenList = max(len(listA), len(listB))
+
+    equalBool = False
+    for i in range(lenList):
+        try:
+            if listA[i] in listB:
+                equalBool = True
+
+            else:
+                return False
+
+
+        except IndexError:
+            print("end of list")
+            break
+
+
+    return equalBool
+
+def removeDuplicates(list):
+    for element in list:
+        if list.count(element) > 1:
+            list.remove(element)
+
+    return list
+
+# main
+def main():
+    exampleListA = [ 1, 4, 9, 16, 9, 7, 4, 9, 11 ]
+    exampleListB = [ 11, 11, 4, 9, 16, 9, 7, 4, 9 ]
+    print("List A", exampleListA)
+    print("List B", exampleListB)
+
+    print("Are they identical:", sameSet(exampleListA, exampleListB))
+
+# PROGRAM RUN
+main()
