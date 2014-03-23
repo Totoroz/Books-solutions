77f9ed731c57b63ffa436e46708447bf78826651
syndbg <syndbe@gmail.com>
2014-03-23 18:35:34 +0200

    Added some more solutions from chapter 8

--- Python For Everyone, Horstmann/Chapter8-Sets-and-Dictionaries/R8.18_test.py
+++ Python For Everyone, Horstmann/Chapter8-Sets-and-Dictionaries/R8.18_test.py
@@ -8,19 +8,22 @@ import unittest
 # main
 class Print_Dictionary_Tests(unittest.TestCase):
     def setUp(self):
-        self.dummy_dictionary = {"example1": 5, "example2": 6, "example3": 7}
+        self.dummy_dictionary = {"A": 8, "D": 3, "B": 15, "F": 2, "C": 6}
 
     def test_print_keys(self):
-        self.assertEqual("example1\nexample2\nexample3", R8_18.print_keys(self.dummy_dictionary))
+        self.assertEqual("A\nB\nC\nD\nF", R8_18.print_keys(self.dummy_dictionary))
 
     def test_print_values(self):
-        self.assertEqual("5\n6\n7", R8_18.print_value(self.dummy_dictionary))
+        self.assertEqual("2\n3\n6\n8\n15", R8_18.print_values(self.dummy_dictionary))
 
-    def test_print_pairs(self):
-        self.assertEqual("('example1', 5)\n('example2', 6)\n('example3', 7)", R8_18.print_pairs(self.dummy_dictionary))
+    def test_print_pairs_sorted(self):
+        self.assertEqual("('A', 8)\n('B', 15)\n('C', 6)\n('D', 3)\n('F', 2)", R8_18.print_pairs(self.dummy_dictionary))
 
     def test_print_average_value(self):
-        self.assertEqual((5+6+7)/3, R8_18.print_average_value(self.dummy_dictionary))
+        self.assertEqual((8+3+15+2+6)/5, R8_18.print_average_value(self.dummy_dictionary))
+
+    def test_print_bar_charts(self):
+        self.assertEqual("A: ********\nB: ***************\nC: ******\nD: ***\nF: **", R8_18.print_bar_charts(self.dummy_dictionary))
 
 
 # PROGRAM RUN
--- Python For Everyone, Horstmann/Chapter8-Sets-and-Dictionaries/R8_18.py
+++ Python For Everyone, Horstmann/Chapter8-Sets-and-Dictionaries/R8_18.py
@@ -1,27 +1,40 @@
-# DOCUMENTATION
+# Given a dictionary
+# gradeCounts = { "A": 8, "D": 3, "B": 15, "F": 2, "C": 6 }
+# write the Python statement(s) to print:
+# a.   all the keys.
+# b.   all the values.
+# c.   all the key and value pairs.
+# d.   all of the key and value pairs in key order.
+# e.   the average value.
+# f.   a chart similar to the following in which each row contains a key followed by a
+# number of asterisks equal to the key’s data value. The rows should be printed in
+# key order, as shown below.
+# A: ********
+# B: ***************
+# C: ******
+# D: ***
+# F: **
 
 
 # FUNCTIONS
 def print_keys(dictionary):
     keys = []
-    for key in dictionary.keys():
+    for key in sorted(dictionary.keys()):
         keys.append(key)
-    keys.sort()
     return "\n".join(keys)
 
 
-def print_value(dictionary):
+def print_values(dictionary):
     values = []
     for value in dictionary.values():
         values.append(value)
-    values = list(map(str, values))
-    values.sort()
+    values = list(map(str, sorted(values)))
     return "\n".join(values)
 
 
 def print_pairs(dictionary):
     pairs = []
-    for key in dictionary:
+    for key in sorted(dictionary):
         pairs.append((key, dictionary[key]))
     pairs.sort()
     return "\n".join(map(str, pairs))
@@ -34,6 +47,14 @@ def print_average_value(dictionary):
         total += 1
         sum += value
     return sum / total
+
+
+def print_bar_charts(dictionary):
+    output_bars = []
+    for key in sorted(dictionary):
+        bar_chart = "*" * dictionary[key]
+        output_bars.append("{}: {}".format(key, bar_chart))
+    return "\n".join(output_bars)
 
 
 # main
