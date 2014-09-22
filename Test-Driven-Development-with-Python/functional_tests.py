e46634b2d0fa22e71cd814ad6980ab940792369b
syndg <syndbe@gmail.com>
2014-09-22 20:37:15 +0300

    Updated functional_tests.py

--- Test-Driven-Development-with-Python/functional_tests.py
+++ Test-Driven-Development-with-Python/functional_tests.py
@@ -1,7 +1,23 @@
+import unittest
+
 from selenium import webdriver
 
 
-browser = webdriver.Firefox()
-browser.get('localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_late(self):
+        self.browser.get('localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+
+
+if __name__ == '__main__':
+    # silences the annoying ResourceWarning
+    unittest.main(warnings='ignore')
