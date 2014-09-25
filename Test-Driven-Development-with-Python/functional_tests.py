bf07fe61839afbfaccc5968b96c14c3b203a9b91
syndg <syndbe@gmail.com>
2014-09-25 16:49:46 +0300

    Added functional tests simulating an input of a to-do item

--- Test-Driven-Development-with-Python/functional_tests.py
+++ Test-Driven-Development-with-Python/functional_tests.py
@@ -1,6 +1,7 @@
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,7 +16,19 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_late(self):
         self.browser.get('localhost:8000')
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(input.get_attribute('placeholder'), 'Entere a to-do item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+
+        self.fail('Finish the test')
 
 
 if __name__ == '__main__':
