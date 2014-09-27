4d33b442e1afb9ec7c013e5655f01941c68dd88f
syndg <syndbe@gmail.com>
2014-09-28 02:03:04 +0300

    Added basic Item model and tests for it

--- Test-Driven-Development-with-Python/lists/tests.py
+++ Test-Driven-Development-with-Python/lists/tests.py
@@ -3,6 +3,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
 
+from .models import Item
 from .views import home_page
 
 
@@ -28,3 +29,18 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html',
             {'new_item_text': 'A new list item'})
         self.assertEqual(expected_html, response.content.decode())
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item.objects.create(text='The first (ever) list item')
+        second_item = Item.objects.create(text='Item the second')
+        saved_items = Item.objects.all()
+
+        self.assertEqual(2, saved_items.count())
+        first_item_saved = saved_items[0]
+        second_item_saved = saved_items[1]
+
+        self.assertEqual(first_item.text, first_item_saved.text)
+        self.assertEqual(second_item.text, second_item_saved.text)
