6c7a8dcec1c797db3450811129f3f4c79ab61a31
syndg <syndbe@gmail.com>
2014-10-01 15:18:01 +0300

    Made functional_tests use a LiveServerTestCase

--- Test-Driven-Development-with-Python/functional_tests/tests.py
+++ Test-Driven-Development-with-Python/functional_tests/tests.py
@@ -1,11 +1,10 @@
-import time
-import unittest
+from django.test import LiveServerTestCase
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -20,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('localhost:8000')
+        self.browser.get(self.live_server_url)
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
@@ -37,8 +36,3 @@ class NewVisitorTest(unittest.TestCase):
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         self.fail('Finish the test')
-
-
-if __name__ == '__main__':
-    # silences the annoying ResourceWarning
-    unittest.main(warnings='ignore')
