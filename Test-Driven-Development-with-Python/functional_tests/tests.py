8e1d510bcaf7c8978ebff673a61b872fcea89ee5
syndg <syndbe@gmail.com>
2014-10-01 16:12:19 +0300

    Refactored and extended tests

--- Test-Driven-Development-with-Python/functional_tests/tests.py
+++ Test-Driven-Development-with-Python/functional_tests/tests.py
@@ -28,11 +28,37 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
+
+        list_url = self.browser.current_url
+        self.assertRegex(list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
+
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+        # new user logs in
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('Use peacock feathers to make a fly', page_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        new_user_url = self.browser.current_url
+        self.assertRegex(new_user_url, '/lists/.+')
+        self.assertNotEqual(list_url, new_user_url)
+
+        page_text = self.browser.find_element_by_id('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('Buy milk', page_text)
 
         self.fail('Finish the test')
