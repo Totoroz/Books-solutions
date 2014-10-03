9deabf48ac9f47ffe9cdda9f73cbcbf774ddfeef
syndg <syndbe@gmail.com>
2014-10-03 22:44:58 +0300

    Added basic layout selenium tests

--- Test-Driven-Development-with-Python/functional_tests/tests.py
+++ Test-Driven-Development-with-Python/functional_tests/tests.py
@@ -88,3 +88,13 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
+
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=5)
+        inputbox.send_keys('Testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=5)
